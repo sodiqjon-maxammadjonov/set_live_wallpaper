cb64026073c7f8ebd29648b814efadae0cd705ae
mukhammadsodikh <138973808+mukhammadsodikh@users.noreply.github.com>
2024-12-11 22:26:51 +0500

    f*ing permission

--- lib/src/function/media/media_function.dart
+++ lib/src/function/media/media_function.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:io';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:set_live_wallpaper/src/model/media/media_from_db_model.dart';
@@ -5,27 +6,38 @@ import 'package:set_live_wallpaper/src/model/media/media_from_db_model.dart';
 class MediaFunctions {
   Future<List<MediaFromDbModel>> loadMedia() async {
     final status = await Permission.storage.request();
+    log('Xotira ruxsati: ${status.isGranted ? "Ruxsat berildi" : "Ruxsat berilmagan"}',name: 'loadMedia');
+
     if (!status.isGranted) {
       throw Exception("Xotira uchun ruxsat berilmagan!");
     }
 
     final directory = Directory('/storage/emulated/0');
+    log('Xotira katalogi: ${directory.path}',name: 'directory');
+
     if (!await directory.exists()) {
+      print('Xotira katalogini yuklashda xatolik!');
       throw Exception("Xotira katalogini yuklashda xatolik!");
     }
 
     final List<MediaFromDbModel> mediaItems = [];
     final List<FileSystemEntity> files = directory.listSync(recursive: true, followLinks: false);
+    log('Fayllar ro\'yxati: ${files.length} ta fayl topildi.');
 
     for (var file in files) {
       final path = file.path.toLowerCase();
+      log('Fayl: $path');
+
       if (path.endsWith('.jpg') || path.endsWith('.jpeg') || path.endsWith('.png')) {
+        log('Rasm topildi: $path');
         mediaItems.add(MediaFromDbModel(path: file.path, type: MediaType.images));
       } else if (path.endsWith('.mp4') || path.endsWith('.mkv')) {
+        log('Video topildi: $path');
         mediaItems.add(MediaFromDbModel(path: file.path, type: MediaType.videos));
       }
     }
 
+    log('Jami media elementlari: ${mediaItems.length}');
     return mediaItems;
   }
 }
